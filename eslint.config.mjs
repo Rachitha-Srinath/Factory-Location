d2f5adf737fa3e447cf541d5c842b773bc1384bd
Rachitha-Srinath <rachsrin90@gmail.com>
2025-07-08 10:10:05 +0200

    Setup ESLint with recommended configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,37 @@
+import js from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      react: pluginReact,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...pluginReact.configs.flat.recommended.rules,
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+    },
+  },
+]);
